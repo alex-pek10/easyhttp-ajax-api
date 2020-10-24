3aff92a32b9272d68baaa86fe02e19bd58ab868a
Alex Myers <alex@pek10.dev>
2020-10-24 13:05:30 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const http = new easyHTTP();
+
+// Get All Posts
+http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(posts);
+  }
+});
+
+// // Get Single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, posts) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// // Create Data - REQUIRED FOR Create/Update BELOW
+// const data = {
+//   title: 'Custom Post',
+//   body: 'This is a custom post',
+// };
+
+// // Create post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function (
+//   err,
+//   post
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// // Update post
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function (
+//   err,
+//   post
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// // Delete post
+// http.delete('https://jsonplaceholder.typicode.com/posts/1', function (
+//   err,
+//   response
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
